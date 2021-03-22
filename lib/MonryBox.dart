213897da33095b5291ea035fbd9ca94b9f075945
siraphak <siraphak.kim@gmail.com>
2021-03-23 01:44:43 +0700

    container ต้นแบบ

---
+++ lib/MonryBox.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+//สร้าง container ต้นแบบ
+class MoneyBox extends StatelessWidget {
+  String title; //ชื่อหมวดหมู่ /รายการ
+  double amount; //จำนวนเงิน
+  Color color; //สีของกล่อง
+  double size; //ขนาดของกล่อง
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10.0),
+      decoration:
+          BoxDecoration(color: color, borderRadius: BorderRadius.circular(10)),
+      height: size,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            title,
+            style: TextStyle(
+                fontSize: 25, color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          Expanded(
+            child: Text(
+              amount.toString(),
+              style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+              textAlign: TextAlign.right,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
